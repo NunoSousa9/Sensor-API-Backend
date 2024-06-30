0bac7047397624a654f5faf8cc6e872d51aef5d9
NunoSousa9 <139639488+NunoSousa9@users.noreply.github.com>
2024-06-30 09:59:01 +0100

    clean

--- src/main/java/com/example/sensorapi/SensorApiApplication.java
+++ src/main/java/com/example/sensorapi/SensorApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.sensorapi;}
+package com.example.sensorapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
